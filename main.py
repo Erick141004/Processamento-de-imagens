9834c98d40335598fba054b09e9b9f5c4b2fb7de
ErickLima <erickcreimberg@gmail.com>
2024-08-20 13:24:34 -0300

    Arrumando codigo

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import PySimpleGUI as sg
+from PIL import Image
+import pathlib
 
-# All the stuff inside your window.
-layout = [  [sg.Text("What's your name?")],
-            [sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+from PySimpleGUI import popup
 
-# Create the Window
-window = sg.Window('Hello Example', layout)
 
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
+def convert_image(input_file_path, output_file_path):
+    img = Image.open(input_file_path)
+    img.save(output_file_path)
+    original_suffix = pathlib.Path(input_file_path).suffix
+    new_suffix = pathlib.Path(output_file_path).suffix
+    return img
 
-    # if user closes window or clicks cancel
-    if event == sg.WIN_CLOSED or event == 'Cancel':
-        break
+if __name__ == "__main__":
+    # ------ Menu Definition ------ #
+    menu_def = [['&File', ['&Open', '&Save', '---', 'Properties', 'E&xit'  ]],
+                ['&Edit', ['Paste', ['Special', 'Normal',], 'Undo'],],
+                ['&Help', ['&About...']]]
 
-    print('Hello', values[0], '!')
+    # All the stuff inside your window.
+    layout = [  [sg.Menu(menu_def)],
+                [sg.Image(key="-IMAGE-")] ]
 
-window.close()
+    # Create the Window
+    window = sg.Window('Hello Example', layout, resizable=True)
+
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+
+        # if user closes window
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == "Open":
+            image = sg.popup_get_file("", no_window=True)
+            converted_image = convert_image(image)
+            window["-IMAGE-"].update(data=converted_image)
+
+        print('Hello', values[0], '!')
+
+    window.close()
