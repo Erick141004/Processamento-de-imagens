1d7e9dcde99df75c717a610270b4ec9426a1d7be
ERICK COSTA REIMBERG DE LIMA <erick.crlima@senacsp.edu.br>
2024-08-20 21:45:58 -0300

    Aula 2 Finalizada

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import PySimpleGUI as sg
 from PIL import Image
+from PIL import ExifTags
+from PIL.TiffTags import TAGS
 import io
+import webbrowser
 import os
 
 image_atual = None
@@ -38,10 +41,50 @@ def info_image():
         tamanho_mb = tamanho_bytes / (1024 * 1024)
         sg.popup(f"Tamanho: {largura} x {altura}\nFormato: {formato}\nTamanho em MB: {tamanho_mb:.2f}")
 
+def info_image_property():
+    global image_atual
+    global image_path
+    
+    #layout_popup = [[sg.Button()]]
+
+    exif = image_atual.getexif()
+    valor = ""
+
+    for tag, value in exif.items():
+        valor += f"{ExifTags.TAGS[tag]} - {value}\n"
+
+    gps_ifd = exif.get_ifd(ExifTags.IFD.GPSInfo)
+    gps_values = get_gps_information(gps_ifd)
+
+    valor += webbrowser.open(f"\nhttps://www.google.com.br/maps/place/{gps_values[0]},{gps_values[1]}")
+    sg.popup(valor)
+
+def get_gps_information(valor):
+    directionLat = valor[1]
+    coordinatesLat = valor[2]
+    directionLong = valor[3]
+    coordinatesLong  = valor[4]
+
+    sumLat = float(coordinatesLat[0]) + (float(coordinatesLat[1])/60) + (float(coordinatesLat[2])/3600) 
+    sumLong = float(coordinatesLong[0]) + (float(coordinatesLong[1])/60) + (float(coordinatesLong[2])/3600) 
+
+    if directionLat == "N":
+        sumLat *= 1
+    elif directionLat == "S":
+        sumLat *= -1
+
+    if directionLong == "W":
+        sumLong *= -1
+    elif directionLong == "E":
+        sumLong *= 1   
+
+    return [sumLat, sumLong]
+                
+
 layout = [
     [sg.Menu([
             ['Arquivo', ['Abrir', 'Salvar', 'Fechar']],
-            ['Sobre a image', ['Informacoes']], 
+            ['Sobre a image', ['Informacoes', '---','Informacoes Especiais']], 
             ['Sobre', ['Desenvolvedor']]
         ])],
     [sg.Image(key='-IMAGE-', size=(800, 600))],
@@ -65,6 +108,8 @@ while True:
                 save_image(arquivo)
     elif event == 'Informacoes':
         info_image()
+    elif event == 'Informacoes Especiais':
+        info_image_property()
     elif event == 'Desenvolvedor':
         sg.popup('Desenvolvido por [Seu Nome] - BCC 6º Semestre')
 
